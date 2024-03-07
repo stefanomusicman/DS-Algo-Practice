eeb8fd9333ab3fed285f3b038cb374007ae65f98
stefano <stef.pro@hotmail.com>
2024-03-07 01:14:23 -0500

    chore: implement simple implementation of queue

--- data_structures/queue/queue.js
+++ data_structures/queue/queue.js
@@ -1,1 +1,37 @@
 // This will be a basic implementation of a queue
+
+// A queue will have essentially 2 methods: Enqueue and Dequeue
+// - Enqueue: Add an element at the end of the queue
+// - Dequeue: Remove the first element of the queue
+
+class Queue {
+    constructor() {
+        this.list = [];
+    }
+
+    Enqueue(item) {
+        this.list = [...this.list, item];
+    }
+
+    Dequeue() {
+        let newList = [];
+
+        for (let i = 1; i < this.list.length; i++) {
+            newList.push(this.list[i]);
+        }
+
+        this.list = newList;
+    }
+}
+
+queue = new Queue();
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(3);
+queue.Enqueue(4);
+console.log(queue.list);
+
+queue.Dequeue();
+queue.Dequeue();
+
+console.log(queue.list);
